b7f7fd7178ca359654af30ed24e5057267fd2187
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-30 23:13:57 +0400

    task failed

---
+++ JsonFileTask/JsonFileTask/Models/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFileTask.Models
+{
+    internal class Methods
+    {
+
+        public void Add()
+        {
+
+        }
+    }
+}
---
+++ JsonFileTask/JsonFileTask/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace JsonFileTask;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+
+        List<string> names = new List<string> { "Ayxan", "john", "doe", "Ağakerim", "David", "Enstain" };
+
+        string result = JsonConvert.SerializeObject(names);
+
+        string path = Path.GetFullPath(Path.Combine(@"..", "..", "..", "Files", "Name.json"));
+        
+
+        using (StreamWriter sw = new StreamWriter(path))
+        {
+
+            sw.WriteLine(result);
+        }
+
+        void Additem(string name)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+               string newNames =  sr.ReadToEnd();
+            }
+
+            names.Add(name);
+            string newResult = JsonConvert.SerializeObject(names);
+            
+             using (StreamWriter sw = new StreamWriter(path))
+           
+            {
+
+                sw.WriteLine(newResult);
+            }
+
+        }
+
+        Additem("abbas");
+
+        void Delete(int index)
+        {
+
+            StreamReader sr = new StreamReader(path);
+            
+                sr.ReadToEnd();
+                 sr.Close();
+
+            names.FindAll(x => x.Equals(x[index]));
+            
+        }
+       
+
+
+
+    }
+}
